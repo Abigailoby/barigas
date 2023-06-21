b44de793f2a3db92a634999a1b4e18219695cbff
fahniM01 <21523074@students.uii.ac.id>
2023-06-22 00:32:29 +0700

    dah didesain

--- menu.php
+++ menu.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Food Kita</title>
-  <link rel = "icon" href ="image/FK4.png" type = "image/x-icon">
+  <title>Barigas</title>
+  <link rel = "icon" href ="image/icon.png" type = "image/x-icon">
 
   <!-- bs cdn -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
